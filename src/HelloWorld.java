f7e250d884d72d21d8f9d677114c933d48ed393c
JANROME <ing.joseantonio.rodriguez@gmail.com>
2021-12-29 15:35:46 -0600

    config file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,4 @@
-package com.devits.hello.world
-
-public class Hellow World {
+public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
     }
